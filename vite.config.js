b3557e0f6a858381418ac046d21b593bfbf74265
Bhargavi V <104492700+Bharg3507C@users.noreply.github.com>
2026-06-28 13:59:02 +0800

    Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['fsa-assignment-zlk5.onrender.com']
+    host: '0.0.0.0',
+    allowedHosts: true
   },
-  preview: {
-    allowedHosts: ['fsa-assignment-zlk5.onrender.com']
-  }
-})
-export default defineConfig({
-  plugins: [react()],
   preview: {
     host: '0.0.0.0',
     allowedHosts: true
